6bdb7027e2c06f38b790515c75250553ebe1cb34
biapasqualinn <104524243+biapasqualinn@users.noreply.github.com>
2022-04-27 16:00:44 -0300

    Update and rename .gitignore to Labirinto.h

---
+++ Labirinto.h
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+#ifndef Labirinto_h
+#define Labirinto_h
+
+class Labirinto {
+
+protected:
+	char labirinto;
+
+public:
+
+	bool posicaoValida(vector<int> p);
+
+	vector<int> SOBE(vector<int> p);
+
+	vector<int> DIREITA(vector<int> p);
+
+	vector<int> DESCE(vector<int> p);
+
+	vector<int> ESQUERDA(vector<int> p);
+
+	bool Livre(vector<string> l, vector<int> p);
+
+	vector<int> procuraEntrada(vector<string> labirinto);
+
+	vector<int> retornaPara(int a, vector<int> p);
+
+	void mostraLabirinto(vector<string> l);
+
+	void mostraPilhaDeAcoes(vector<int> a);
+
+	vector<string> alteraLabirinto(vector<string> l, vector<int> p, char c);
+};
+
+#endif
